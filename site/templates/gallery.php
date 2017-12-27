fec5caab03ddba093a9823d669030a2788d0f6ef
webmaster-vff <webmaster@vff-geisweid.de>
2017-12-27 16:32:56 +0100

    Kleinere Fehler behoben

--- site/templates/gallery.php
+++ site/templates/gallery.php
@@ -7,24 +7,26 @@
       <h2><?= $page->title()->html() ?></h2>
       <hr />
       <div class="text">
-       <?= $page->text()->kirbytext() ?>
-      </div>
-      <?php $i=1 ?>
-      <?php foreach ($page->images() as $image): ?>
-        <?php if ($i % 2 <> 0): ?>
-         <figure>
-        <?php endif ?>
-          <a class="noline" href="<?= $image->url() ?>" data-lightbox="<?= $page->title() ?>" data-title="<?= $page->title() ?>">
-            <!--<img src="<?= $image->url() ?>" alt="" width="360"/>-->
-            <?= thumb($image, array('width' => 360)) ?>
-          </a>
-        <?php if ($i % 2 == 0): ?>
-         </figure>
-       <?php endif ?>
-       <?php $i++ ?>
-      <?php endforeach ?>
-    </div>
+        <?= $page->text()->kirbytext() ?>
 
+        <?php $i=1 ?>
+        <?php foreach ($page->images() as $image): ?>
+          <?php if ($i % 2 <> 0): ?>
+            <figure>
+            <?php endif ?>
+            <a class="noline" href="<?= $image->url() ?>" data-lightbox="<?= $page->title() ?>" data-title="<?= $page->title() ?>">
+              <!--<img src="<?= $image->url() ?>" alt="" width="360"/>-->
+              <?= thumb($image, array('width' => 360)) ?>
+            </a>
+            <?php if ($i % 2 == 0): ?>
+              </figure>
+            <?php endif ?>
+            <?php $i++ ?>
+        <?php endforeach ?>
+      <hr />
+      </div>
+
+    </div>
     <?php snippet('sidebar') ?>
 
 	</div>
